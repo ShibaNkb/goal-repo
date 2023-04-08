ba109a6f1b9c8e0a5a88fb41ac43f4151289fe76
saba <shiba.nkb@gmail.com>
2023-04-08 14:41:14 +0400

    Phase 1

--- UserInput-Return.py
+++ UserInput-Return.py
@@ -1,5 +1,7 @@
 # * Input function is inbuilt in python.
 # * Provide a parameter that tells a user what to do.
+# * Return statements hold a value that can be put in a variable and called.
+
 
 to_minutes = 24 * 60
 to_seconds = 24 * 60 * 60
@@ -7,8 +9,11 @@ unit_name = 'seconds'
 unit_name1 = 'minutes'
 
 def days_to_units (num_of__days, custome_msg):
-    print(f'{num_of__days} days are {num_of__days * to_seconds} {unit_name}.')
-    print(custome_msg)
+    return f'{num_of__days} days are {num_of__days * to_seconds} {unit_name}.'
    
+   
+my_var = days_to_units(20)
+print(my_var)
+
 user_input = input('Hey user, enter a number of days and i will convert it to hours.\n')
 print(user_input)
